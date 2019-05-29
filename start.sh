2d8dc53735beb0fe2c110c778dfb4caddc2fa5b3
FacuM <facumo.fm@gmail.com>
2019-05-29 08:02:15 -0300

    Switch to the required repo, drop Bash build script.

--- start.sh
+++ start.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
-REMOTE_HOST='https://github.com'
-REMOTE_PATH='FacuM'
-REMOTE_REPO='python-duckduckgo'
-if [ ! -d "$HOME"'/'"$REMOTE_REPO" ]
-then
-  git clone "$REMOTE_HOST"'/'"$REMOTE_PATH"'/'"$REMOTE_REPO" "$HOME"'/'"$REMOTE_REPO"
-  PRE_PWD="$PWD"
-  cd "$HOME"'/'"$REMOTE_REPO"
-  python3 setup.py install --user
-  cd "$PRE_PWD"
-fi
 python3 -m pip install -r requirements.txt --user
 python3 main.py
